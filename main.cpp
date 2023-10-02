d3f20c56b9044ffe230cc85f0c2f389a03702371
S-Onuki <k022g0020@g.neec.ac.jp>
2023-10-02 15:25:42 +0900

    Min関数を数値型のみを通すように変更

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 ﻿#include <iostream>
 #include <stdint.h>
 #include <type_traits>
+#include <concepts>
+#include <string>
 
-//SFINAEをやろうとした残骸
-//template <typename T>
-//struct is_number : std::integral_constant<std::is_integral<T>::value || std::is_floating_point<T>::value> {};
+// 数値型である場合のみ通る
+template<typename T>
+concept Number = requires{ std::is_integral<T>::value_type; std::is_floating_point<T>::value_type; };
 
 /// @brief min関数
 /// @tparam T 型名
 /// @param A 数値A
 /// @param B 数値B
 /// @return 小さいほうの値
-template <typename T>
+template <Number T>
 T Min(T A, T B) {
 	return (A < B) ? A : B;
 }
