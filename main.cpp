bf16e07544ea58722c2c31241a56fd37dcb37d4a
S-Onuki <k022g0020@g.neec.ac.jp>
2023-10-18 17:35:17 +0900

    6個作った

--- main.cpp
+++ main.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <stdint.h>
 
-template <typename T>
+template <typename T1, typename T2>
 class ValuePair
 {
 public:
-	ValuePair(T a, T b);
+	ValuePair(T1 a, T2 b);
 	~ValuePair() = default;
 
-	const T &Min() const {
-		return std::min<T>(a_, b_);
+	const T1 &Min() const {
+		return std::min<T1>(a_, static_cast<T1>(b_));
 	}
 
-	friend std::ostream &operator<<(std::ostream &os, const ValuePair<T> &obj) {
-		os << "ValuePair<" << typeid(T).name() << ">: " << obj.a_ << ", " << obj.b_ << " => Min(" << obj.Min() << ")";
+	friend std::ostream &operator<<(std::ostream &os, const ValuePair<T1, T2> &obj) {
+		os << "ValuePair<" << typeid(T1).name() << ", " << typeid(T2).name() << ">: " << obj.a_ << ", " << obj.b_ << " => Min<" << typeid(T1).name() << ">(" << obj.Min() << ")";
 		return os;
 	}
 
 private:
-	T a_;
-	T b_;
+	T1 a_;
+	T2 b_;
 
 };
-template <typename T>
-ValuePair<T>::ValuePair(T a, T b) : a_(a), b_(b) {}
+template <typename T1, typename T2>
+ValuePair<T1, T2>::ValuePair(T1 a, T2 b) : a_(a), b_(b) {}
 
 int main(void) {
-	std::cout << ValuePair{ 1, 5 } << std::endl;
-	std::cout << ValuePair{8.f, 5.f} << std::endl;
-	std::cout << ValuePair{153.0, 325.0} << std::endl;
+	std::cout << ValuePair{ 1, 5.f } << std::endl;
+	std::cout << ValuePair{ 153, 325.0 } << std::endl;
+
+	std::cout << ValuePair{ 8.f, 5 } << std::endl;
+	std::cout << ValuePair{ 153.f, 325.0 } << std::endl;
+
+	std::cout << ValuePair{ 1.0, 5 } << std::endl;
+	std::cout << ValuePair{ 8.0, 5.f } << std::endl;
 
 	return 0;
 }
