c905472dbcd1926905aeda1de7d0d0366934710b
S-Onuki <k022g0020@g.neec.ac.jp>
2024-01-22 10:32:36 +0900

    06_02完了

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <cstdio>
+#include <thread>
+
+void AddAndPrint(uint32_t *value) {
+	if (value) {
+		std::cout << "thread " << (*value)++ << std::endl;
+	}
+}
 
 int main(void) {
 
-	std::printf("あいうえお\n");
+	uint32_t number_ = 1u;
+
+	std::thread tr1{ AddAndPrint ,&number_ };
+	tr1.join();
+
+	std::thread tr2{ AddAndPrint ,&number_ };
+	tr2.join();
+
+	std::thread tr3{ AddAndPrint ,&number_ };
+	tr3.join();
+
 	return 0;
 }
