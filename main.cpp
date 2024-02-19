14d594079b8abab4c6604547bc8b0a1510fe749c
S-Onuki <k022g0020@g.neec.ac.jp>
2024-02-20 01:26:52 +0900

    同速にまでもってった

--- main.cpp
+++ main.cpp
@@ -5,39 +5,15 @@
 
 static inline void TransponeArray(std::array<__m128, 2u> *data)
 {
-	/*static const __m256i indices = _mm256_set_epi32(5, 2, 7, 4, 1, 6, 3, 0);
+	constexpr int32_t mask = 108; // 0, 3, 2, 1
 
-	*data = _mm256_permutevar8x32_ps(*data, indices);*/
+	constexpr int32_t blendMask = 0b0100;
 
-	//_mm256_storeu_ps(data, result);
-
-	// static const uint32_t mask = _MM_SHUFFLE(1, 2, 3, 0); // 0,3,6,1,4,7,2,5
-
-	// __m256 temp1 = _mm256_permute_ps(data, mask);
-	// __m256 temp2 = _mm256_permute2f128_ps(temp1, temp1, 1);
-
-	// data = _mm256_blend_ps(temp1, temp2, 0b01000100);
-
-	/*__m128 input_low = _mm_loadu_ps(data);
-	__m128 input_high = _mm_loadu_ps(data.data() + 4);*/
-
-	constexpr uint32_t mask = _MM_SHUFFLE(1, 2, 3, 0); // 0, 3, 2, 1
-
+	data->at(1) = _mm_permute_ps(data->at(1), mask);
 	__m128 temp = _mm_permute_ps(data->at(0), mask);
-	data->at(1) = _mm_shuffle_ps(data->at(1), data->at(1), mask);
 
-
-	// Swap the second elements
-	data->at(0) = _mm_blend_ps(temp, data->at(1), 0b0100);
-	data->at(1) = _mm_blend_ps(data->at(1), temp, 0b0100);
-
-	//__m128 temp2_low = _mm_shuffle_ps(temp1_low, temp1_low, _MM_SHUFFLE(2, 3, 0, 1)); // Swap low elements
-	//__m128 temp2_high = _mm_shuffle_ps(temp1_high, temp1_high, _MM_SHUFFLE(2, 3, 0, 1)); // Swap high elements
-
-	//data->at(0) = _mm_shuffle_ps(temp1_low, temp2_low, _MM_SHUFFLE(2, 0, 2, 0));
-	//data->at(1) = _mm_shuffle_ps(temp1_high, temp2_high, _MM_SHUFFLE(2, 0, 2, 0));
-
-
+	data->at(0) = _mm_blend_ps(temp, data->at(1), blendMask);
+	data->at(1) = _mm_blend_ps(data->at(1), temp, blendMask);
 }
 
 struct Matrix3x3 {
@@ -49,12 +25,13 @@ struct Matrix3x3 {
 			this->m[0][1], this->m[1][1], this->m[2][1],
 			this->m[0][2], this->m[1][2], this->m[2][2]} };
 	}
+
 	inline Matrix3x3 TransponeSIMD() const
 	{
-		union/* alignas(16) */SimdVec {
+		union alignas(16) SimdVec {
 			std::array<__m128, 2u> simd;
 			std::array<float, 8u> arr;
-			//Matrix3x3 m;
+			// Matrix3x3 m;
 		};
 
 		Matrix3x3 result;
@@ -63,23 +40,23 @@ struct Matrix3x3 {
 		// Load the data into a single __m128
 		SimdVec calc = { _mm_load_ps(m.data()->data()), _mm_load_ps(m.data()->data() + 4u) };
 
-		//SimdVec calc = { .m = *this };
+		// SimdVec calc = { .m = *this };
 		TransponeArray(&calc.simd);
 		//// Transpose using shuffle
 		//__m128 shuf1 = _mm_shuffle_ps(calc[0], calc[0], _MM_SHUFFLE(3, 1, 2, 0));
 		//__m128 shuf2 = _mm_shuffle_ps(calc[1], calc[1], _MM_SHUFFLE(3, 1, 2, 0));
 
 		//// Concatenate the shuffles to get the final transposed result
-		//result.m[0][0] = _mm_cvtss_f32(calc[0]);
-		//result.m[0][1] = _mm_cvtss_f32(shuf1);
-		//result.m[0][2] = _mm_cvtss_f32(_mm_shuffle_ps(shuf2, shuf1, _MM_SHUFFLE(2, 0, 2, 0)));
+		// result.m[0][0] = _mm_cvtss_f32(calc[0]);
+		// result.m[0][1] = _mm_cvtss_f32(shuf1);
+		// result.m[0][2] = _mm_cvtss_f32(_mm_shuffle_ps(shuf2, shuf1, _MM_SHUFFLE(2, 0, 2, 0)));
 
-		//result.m[1][0] = _mm_cvtss_f32(_mm_shuffle_ps(calc[0], shuf1, _MM_SHUFFLE(2, 0, 2, 0)));
-		//result.m[1][1] = _mm_cvtss_f32(shuf2);
-		//result.m[1][2] = _mm_cvtss_f32(_mm_shuffle_ps(shuf1, shuf2, _MM_SHUFFLE(3, 1, 3, 1)));
+		// result.m[1][0] = _mm_cvtss_f32(_mm_shuffle_ps(calc[0], shuf1, _MM_SHUFFLE(2, 0, 2, 0)));
+		// result.m[1][1] = _mm_cvtss_f32(shuf2);
+		// result.m[1][2] = _mm_cvtss_f32(_mm_shuffle_ps(shuf1, shuf2, _MM_SHUFFLE(3, 1, 3, 1)));
 
-		//result.m[2][0] = _mm_cvtss_f32(_mm_shuffle_ps(shuf1, calc[1], _MM_SHUFFLE(3, 1, 3, 1)));
-		//result.m[2][1] = _mm_cvtss_f32(_mm_shuffle_ps(shuf2, calc[1], _MM_SHUFFLE(3, 1, 3, 1)));
+		// result.m[2][0] = _mm_cvtss_f32(_mm_shuffle_ps(shuf1, calc[1], _MM_SHUFFLE(3, 1, 3, 1)));
+		// result.m[2][1] = _mm_cvtss_f32(_mm_shuffle_ps(shuf2, calc[1], _MM_SHUFFLE(3, 1, 3, 1)));
 
 		_mm_store_ps(result.m.data()->data(), calc.simd[0]);
 		_mm_store_ps(result.m.data()->data() + 4u, calc.simd[1]);
@@ -101,7 +78,7 @@ struct Matrix3x3 {
 
 int main()
 {
-	const uint32_t iterations = 10000000u;
+	constexpr uint32_t iterations = 100000000u;
 
 	Matrix3x3 matrix{ .m{
 		0, 1, 2,
@@ -113,7 +90,7 @@ int main()
 	Matrix3x3 result;
 	// Benchmark TransponeSIMD
 	auto start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < iterations; ++i) {
+	for (uint32_t i = 0u; i < iterations; ++i) {
 		result = matrix.TransponeSIMD();
 	}
 
@@ -124,7 +101,7 @@ int main()
 
 	// Benchmark Transpone
 	start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < iterations; ++i) {
+	for (uint32_t i = 0u; i < iterations; ++i) {
 		result = matrix.Transpone();
 	}
 	end = std::chrono::high_resolution_clock::now();
