4a564e4e7d46941fb08dad4747085cee644d2b1a
S-Onuki <k022g0020@g.neec.ac.jp>
2024-03-14 02:38:42 +0900

    静的にアクセスできる文字列でのマップ

--- main.cpp
+++ main.cpp
@@ -1,40 +1,59 @@
+#include <map>
+#include <string>
 #include <iostream>
-#include <cstdint>
 #include <array>
-#include <ostream>
+#include <string_view>
 
-template <size_t Size>
+// コンパイル時定義固定文字列
+template <uint32_t Size>
 struct ConstExprString {
-	inline static constexpr uint32_t length_ = Size - 1;
+	static constexpr uint32_t length_ = Size - 1;
 	std::array<char, Size> buf_;
 
-	consteval ConstExprString(const char(&s_literal)[Size]) noexcept {
-		for (int i = 0; i < Size; ++i) {
+	// 文字列リテラルから推論を行うためのコンストラクタ
+	constexpr ConstExprString(const char(&s_literal)[Size]) {
+		for (uint32_t i = 0u; i < Size; i++) {
 			buf_[i] = s_literal[i];
 		}
-		buf_[length_] = '\n';
 	}
 };
 
-template <ConstExprString V>
-struct StringClass {
-	inline static constexpr ConstExprString value = V;
-public:
-	inline static constexpr const char *const c_str() noexcept { return value.buf_.data(); }
-	inline static constexpr friend std::ostream &operator<< (std::ostream &ost, const StringClass str) { return ost << str.c_str(); }
+// 静的固定文字列
+template <ConstExprString Str>
+struct StaticString {
+	static constexpr auto str_ = Str;
+
+	constexpr std::string_view view() const noexcept { return { str_.data(), str_.length_ + 1 }; }
+
+	constexpr const char *c_str() const noexcept { return str_.buf_.data(); }
+
+	constexpr bool operator<(const StaticString &other) const noexcept {
+		return std::string_view{ c_str(), str_.length_ } < std::string_view{ other.c_str(), other.str_.length_ };
+	}
 };
 
-template <typename T, ConstExprString V = T::value>
-	requires(std::same_as<T, StringClass<V>>)
-void printString(const T &str) {
-	std::cout << "String from template function: " << str << std::endl;
-}
+// StaticStringをキーとするコンテナの実装
+template <typename T, ConstExprString... StaticStrings>
+struct StaticStringMap {
+	inline static constexpr std::tuple<std::pair<StaticString<StaticStrings>, T>...> data_ = {
+		std::make_tuple(std::make_pair(StaticString<StaticStrings>{}, T{})...)
+	};
+
+	// StaticStringから値を取得する
+	template <ConstExprString Str>
+	static constexpr const T &get() {
+		return std::get<std::pair<StaticString<Str>, T>>(data_).second;
+	}
+};
 
 int main() {
-	StringClass<"test"> str1;
+	// コンテナの初期化
+	using MapType = StaticStringMap<int, "hello", "world">;
+	constexpr auto value1 = MapType::get<"hello">();
+	constexpr auto value2 = MapType::get<"world">();
 
-	// StringClassの文字列用のテンプレート関数呼び出し
-	printString(str1);
+	std::cout << "Value for key 'hello': " << value1 << std::endl;
+	std::cout << "Value for key 'world': " << value2 << std::endl;
 
 	return 0;
 }
