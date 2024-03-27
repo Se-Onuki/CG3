d0dbd097edf14b231cd103b097af6baf78267b7a
S-Onuki <k022g0020@g.neec.ac.jp>
2024-03-27 12:26:31 +0900

    関数をtupleとして渡すようにしてみる

--- main.cpp
+++ main.cpp
@@ -1,79 +1,45 @@
-#include <map>
-#include <string>
 #include <iostream>
-#include <array>
-#include <string_view>
+#include <tuple>
+#include <concepts>
 
-// コンパイル時定義固定文字列
-template <uint32_t Size>
-struct ConstExprString {
-	static constexpr uint32_t length_ = Size - 1;
-	std::array<char, Size> buf_;
+// Example ISystem interface
+struct ISystem {
+	/* virtual void Update() = 0;
+	 virtual ~ISystem() = default;*/
+};
 
-	// 文字列リテラルから推論を行うためのコンストラクタ
-	constexpr ConstExprString(const char(&s_literal)[Size]) {
-		for (uint32_t i = 0u; i < Size; i++) {
-			buf_[i] = s_literal[i];
-		}
+// Sample implementations of ISystem
+struct System1 : public ISystem {
+	void Update() const {
+		std::cout << "System1 Update\n";
 	}
 };
 
-// 静的固定文字列
-template <ConstExprString Str>
-struct StaticString {
-	static constexpr auto str_ = Str;
-
-	constexpr std::string_view view() const noexcept { return { str_.data(), str_.length_ + 1 }; }
-
-	constexpr const char *c_str() const noexcept { return str_.buf_.data(); }
-
-	template <ConstExprString T>
-	constexpr bool operator<(const StaticString<T> &other) const noexcept {
-		return c_str() < other.c_str();
+struct System2 : public ISystem {
+	void Update() const {
+		std::cout << "System2 Update\n";
 	}
 };
 
-// StaticStringをキーとするコンテナの実装
-template <typename T, ConstExprString... StaticStrings>
-struct StaticStringMap {
-	inline static std::tuple<std::pair<StaticString<StaticStrings>, T>...> data_ = {
-		std::make_tuple(std::make_pair(StaticString<StaticStrings>{}, T{})...)
-	};
+// Concept to check if a type is derived from ISystem
+template<typename T>
+concept IsSystem = std::is_base_of_v<ISystem, T>;
 
-	// StaticStringから値を取得する
-	template <ConstExprString Str>
-	static constexpr const T &get() {
-		return std::get<std::pair<StaticString<Str>, T>>(data_).second;
+// Function to update systems in tuple
+template<std::size_t Index = 0, IsSystem... Systems>
+void UpdateSystems(const std::tuple<Systems...> &systems) {
+	if constexpr (Index < sizeof...(Systems)) {
+		std::get<Index>(systems).Update();
+		UpdateSystems<Index + 1>(systems);
 	}
-
-	// StaticStringに対応する値を設定する
-	template <ConstExprString Str>
-	static constexpr void set(const T &value) {
-		std::get<std::pair<StaticString<Str>, T>>(data_).second = value;
-	}
-
-	// StaticStringに対応する値を設定する
-	template <ConstExprString Str>
-	static constexpr void set(T &&value) {
-		std::get<std::pair<StaticString<Str>, T>>(data_).second = std::move(value);
-	}
-};
+}
 
 int main() {
-	// コンテナの初期化
-	using MapType = StaticStringMap<std::string, "hello", "world">;
-	const auto &value1 = MapType::get<"hello">();
-	const auto &value2 = MapType::get<"world">();
+	// Create a tuple with systems
+	std::tuple<System1, System2> systems;
 
-	std::cout << "Value for key 'hello': " << value1 << std::endl;
-	std::cout << "Value for key 'world': " << value2 << std::endl;
-
-	// 値の変更
-	MapType::set<"hello">("Bonjour");
-	MapType::set<"world">("Monde");
-
-	std::cout << "Updated value for key 'hello': " << MapType::get<"hello">() << std::endl;
-	std::cout << "Updated value for key 'world': " << MapType::get<"world">() << std::endl;
+	// Call Update on each system in the tuple
+	UpdateSystems(systems);
 
 	return 0;
 }
